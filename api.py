3fef8a30685fc948013c4ed5bbef90a872feaf6e
banchanbetispro <khanhphannam7@gmail.com>
2024-01-17 21:59:09 +0700

    No longer need pretty printer

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from requests import get
-from pprint import PrettyPrinter
+#from pprint import PrettyPrinter
 
 #Initialize printer
-printer = PrettyPrinter()
+#printer = PrettyPrinter()
 data = get("https://www.randomlists.com/data/words.json").json()["data"]
